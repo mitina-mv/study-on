b4a12598ec3d3a84a4acddb113944ce3c439baf1
mitina-mv <mit.maria2019@yandex.ru>
2024-04-25 20:54:47 +0000

    feat: метод для отправки запроса на авторизацию на клиенте

--- src/Service/BillingRequstService.php
+++ src/Service/BillingRequstService.php
@@ -7,5 +7,40 @@ use JsonException;
 
 class BillingRequstService
 {
+    public static function post(
+        string $url,
+        string $data = null,
+        $token = null
+    ) : array {
+        $curl = curl_init($url);
+        $options = [
+            'Content-Type: application/json',
+            'Accept: application/json'
+        ];
 
+        if ($token !== null) {
+            $options[] = 'Authorization: Bearer ' . $token;
+        }
+
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $options);
+
+        if ($data !== null) {
+            curl_setopt(
+                $curl,
+                CURLOPT_POSTFIELDS,
+                $data
+            );
+        }
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        if ($response === false) {
+            throw new BillingUnavailableException('Сервис временно недоступен.');
+        }
+        
+        return json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+    }
 }
