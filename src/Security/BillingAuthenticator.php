f7212c456084c4746ddd193544066b0fcc63f888
mitina-mv <mit.maria2019@yandex.ru>
2024-04-26 08:46:57 +0000

    feat: добавлен метод авторизации (необходимо дописать)

--- src/Security/BillingAuthenticator.php
+++ src/Security/BillingAuthenticator.php
@@ -2,17 +2,24 @@
 
 namespace App\Security;
 
+use App\Exception\BillingUnavailableException;
+use App\Service\BillingClient;
+use Exception;
+use JsonException;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
@@ -22,22 +29,48 @@ class BillingAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
-    {
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private BillingClient $billingClient,
+    ) {
     }
 
-    public function authenticate(Request $request): Passport
+    public function authenticate(Request $request) : SelfValidatingPassport
     {
-        $email = $request->getPayload()->getString('email');
+        $email = $request->request->get('email', null);
+        $password = $request->request->get('password', null);
 
-        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
+        // запрос на авторизацию
+        $credentials = json_encode([
+            'username' => $email,
+            'password' => $password,
+        ], JSON_THROW_ON_ERROR);
 
-        return new Passport(
-            new UserBadge($email),
-            new PasswordCredentials($request->getPayload()->getString('password')),
+        try {
+            $response = $this->billingClient->authenticate($credentials);
+        } catch (BillingUnavailableException | JsonException $e) {
+            throw new Exception('Произошла ошибка во время авторизации: ' . $e->getMessage());
+        }
+        if (isset($response['code'])) {
+            throw new AuthenticationException($response['message']);
+        }
+
+        // получаем всю информацию о текущем пользователе
+        $loaderUser = function ($response): UserInterface {
+            try {
+            } catch (BillingUnavailableException | JsonException $e) {
+            }
+
+            $user = new User();
+            $user->setApiToken($response['token']);
+
+            return $user;
+        };
+
+        return new SelfValidatingPassport(
+            new UserBadge($email, $loaderUser),
             [
-                new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
-                new RememberMeBadge(),
+                new CsrfTokenBadge('authenticate', $request->get('_csrf_token')),
             ]
         );
     }
