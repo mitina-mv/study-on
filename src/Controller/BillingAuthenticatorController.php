097027214d8123ba945c08c5f117d49714f6a5ba
mitina-mv <mit.maria2019@yandex.ru>
2024-04-26 12:27:42 +0000

    feat: начала писать метод регистрации

--- src/Controller/BillingAuthenticatorController.php
+++ src/Controller/BillingAuthenticatorController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\UserRegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class BillingAuthenticatorController extends AbstractController
@@ -13,7 +16,7 @@ class BillingAuthenticatorController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
+        //     return $this->redirectToRoute('app_course_index');
         // }
 
         // get the login error if there is one
@@ -24,9 +27,30 @@ class BillingAuthenticatorController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
+    #[IsGranted('ROLE_USER')]
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new \LogicException(
+            'This method can be blank - it will be intercepted by the logout key on your firewall.'
+        );
+    }
+
+    #[Route(path: '/registration', name: 'app_registration')]
+    public function registration(Request $request): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_course_index');
+        }
+
+        $form = $this->createForm(UserRegistrationType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $credentials = json_encode([
+                'username' => $form["email"]->getData(),
+                'password' => $form["password"]->getData(),
+            ]);
+        }
     }
 }
---
+++ src/Form/UserRegistrationType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UserRegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Электронная почта',
+                'attr' => [
+                    'class' => 'form-control mb-3',
+                    'placeholder' => 'Электронная почта',
+                ],
+                'constraints' => [
+                    new Email(message: 'Введенный адрес электронной почты невалиден.'),
+                    new NotBlank(message: 'Почта не может быть пустой.'),
+                ],
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'mapped' => false,
+                'options' => ['attr' => ['class' => 'mb-3 w-100']],
+                'attr' => ['autocomplete' => 'new-password'],
+                'invalid_message' => 'Введенные пароли не совпадают.',
+                'first_options'  => [
+                    'label' => 'Пароль',
+                    'placeholder' => 'Минимум 6 символов',
+                    'class' => 'form-control mb-3',
+                ],
+                'second_options' => [
+                    'label' => 'Подтвердите пароль',
+                    'class' => 'form-control mb-3',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Пароль не может быть пустым.',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Пароль должен содержать минимум 6 символов',
+                        'max' => 255,
+                    ]),
+                ],
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
